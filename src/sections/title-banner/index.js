8718231a7c8f77f7b3426f0547b7e49cdde88b61
Ross Leitch <professional.balbatross@gmail.com>
2020-12-04 17:14:23 +1300

    Title banner and header adjustments

--- src/sections/title-banner/index.js
+++ src/sections/title-banner/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import {
+  ExpandMore
+} from '@material-ui/icons';
+
+import { 
+  Typography,
+  Button
+} from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import LoginBox from '../../components/login-box';
 import './index.css';
@@ -7,9 +15,25 @@ import './index.css';
  function TitleBanner(props){
    return (
      <div className="title-banner">
-     <div className="title-video">
-        <video controls src="/consenttalk.mp4" />
-      </div>
+
+       <div className="title-video">
+         <Typography 
+           style={{marginBottom: 12}} 
+           variant="h4">Latest Video</Typography>
+         <video 
+           controls 
+           src="/consenttalk.mp4" />
+         <Button 
+           style={{marginTop: 12}} 
+           variant="outlined" 
+           size="large" 
+           color="primary"
+          onClick={() => props.history.push('/content')} >See More</Button>
+       </div>
+
+       <div className="title-more">
+          <ExpandMore /> 
+       </div>
     </div>
   );
  }
